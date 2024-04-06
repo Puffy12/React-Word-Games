90f4e3976855f1790042534a47efed482db5ae61
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-06 13:53:54 -0700

    Added Tailwind/ Finshed Settup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import words from "./wordList.json"
+import { useState } from "react"
+import words from "./wordList.json";
+import "./index.css";
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)]
 }
 
-const [wordToGuess, setWordToGuess] = useState(getWord)
-const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-
-
 function App() {
-  return (
-    <h1>HETTETsdfdsfET</h1>
-    
-  )
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  
+  return <div className="max-w-800 flex flex-col gap-8 mx-auto items-center">
+            Hi
+          </div>
 }
 
 export default App
