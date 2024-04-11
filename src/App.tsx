648a60103e86e073750ad4675d483393c4049b68
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-10 18:07:01 -0700

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,17 @@
-import { useCallback, useEffect, useState } from "react"
-import { useSignal, useComputed, useSignalEffect, computed } from "@preact/signals-react";
-import words from "./wordList.json";
-import "./index.css";
-import { HangmanDrawing } from "./HangmanDrawing";
-import HangmanWord from "./HangmanWord";
-import { Keyboard }from "./HangmanKeyboard";
-
-function getWord() {
-  return words[Math.floor(Math.random() * words.length)]
-}
-
-
+import React from "react";
+import { BrowserRouter, Routes , Route } from "react-router-dom";
+import HomeMenu from "./components/Home/Home";
+import Hangman from "./components/Hangman/Hangman";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(getWord)
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-
-  const incorrectLetters = guessedLetters.filter(
-    letter => !wordToGuess.includes(letter)
-  )
-
-  const isLoser = incorrectLetters.length >= 6 //6 body parts
-  const isWinner = wordToGuess
-    .split("")
-    .every(letter => guessedLetters.includes(letter))
-
-  const addGuessedLetter = useCallback(
-    (letter: string) => {
-      if (guessedLetters.includes(letter) || isLoser || isWinner) return
-
-      setGuessedLetters(currentLetters => [...currentLetters, letter])
-    },
-    [guessedLetters, isWinner, isLoser]
-  )
-
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (!key.match(/^[a-z]$/)) return
-
-      e.preventDefault()
-      addGuessedLetter(key)
-    }
-
-    document.addEventListener("keypress", handler)
-
-    return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [guessedLetters])
-
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (key !== "Enter") return
-
-      e.preventDefault()
-      setGuessedLetters([])
-      setWordToGuess(getWord())
-    }
-
-    document.addEventListener("keypress", handler)
-
-    return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [])
-
   return (
-    <div className="max-w-800 flex flex-col gap-8 mx-auto items-center">
-      {wordToGuess}
-      <div className="text-2xl text-center">
-        {isWinner && "Winner! - Refresh to try again"}
-        {isLoser && "Nice Try - Refresh to try again"}
-      </div>
-      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-      <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
-      <div className="self-stretch px-10 ">
-        <Keyboard 
-          disabled={isWinner || isLoser}
-          activeLetters={guessedLetters.filter(letter =>
-            wordToGuess.includes(letter)
-          )}
-          inactiveLetters={incorrectLetters}
-          addGuessedLetter={addGuessedLetter}
-          />
-      </div>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route index element={ <HomeMenu/> } />
+        <Route path="/hangman" Component={Hangman} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { faBoxOpen, faW, faFingerprint, faMagnifyingGlass, faMemory, faTerminal, faHouse, faQuestionCircle, faS, faH } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./Home.css"; // Import your CSS file
+
+const HomeMenu = () => {
+  return (
+    <div className="App">
+      <div>
+        <div className="header-container">
+          <h1>Word Games</h1>
+        </div>
+        <div className="challenge-container challenge-grid">
+          <div className="challenge-row top-row">
+            <Link className="challenge-item " to={"/wordle"}>
+              <FontAwesomeIcon icon={faW} size="3x" className="icon" />
+            </Link>
+            <Link className="challenge-item " to={"/crossword"}>
+              <FontAwesomeIcon icon={faS} size="3x" className="icon" />
+            </Link>
+            <Link className="challenge-item " to={"/hangman"}>
+              <FontAwesomeIcon icon={faH} size="3x" className="icon" />
+            </Link>
+          </div>
+          <div className="challenge-row bottom-row">
+            <Link className="challenge-item " to={"/"}>
+              <FontAwesomeIcon icon={faHouse} size="3x" className="icon" />
+            </Link>
+            <Link className="challenge-item " to={"/about"}>
+              <FontAwesomeIcon icon={faQuestionCircle} size="3x" className="icon" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeMenu;
