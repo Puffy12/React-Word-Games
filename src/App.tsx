43eabf59db8840ec15b3fa98a0b169f90feae849
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-20 12:03:03 -0700

    Vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { BrowserRouter, Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeMenu from "./components/Home/Home";
 import Hangman from "./components/Hangman/Hangman";
-import wordle from "./components/Wordle/wordle";
-import about from "./components/About/About";
-import crossword from "./components/Crossword/crossword";
+import Wordle from "./components/Wordle/wordle"; 
+import About from "./components/About/About";
+import Crossword from "./components/Crossword/crossword"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={ <HomeMenu/> } />
-        <Route path="/hangman" Component={Hangman} />
-        <Route path="/wordle" Component={wordle} />
-        <Route path="/about" Component={about} />
-        <Route path="/crossword" Component={crossword} />
-
+        <Route index element={<HomeMenu />} />
+        <Route path="/hangman" element={<Hangman />} />
+        <Route path="/wordle" element={<Wordle />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/crossword" element={<Crossword />} />
       </Routes>
     </BrowserRouter>
   );
