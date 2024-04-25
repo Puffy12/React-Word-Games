84e7a670fd49fa26676977211d2fa5012e164955
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-25 16:27:42 -0700

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,10 @@
 import Footer from '../SideBar/footer'
 import Sidebar from '../SideBar/sidebar'
+import { BsArrowRight } from "react-icons/bs";
+
 
 function about() {
+  
   return (
 <div className='flex flex-col min-h-screen bg-gray-600 text-gray-300'>
   <Sidebar />
@@ -41,12 +44,74 @@ function about() {
         <span className="font-medium"> Join us as we continue to innovate and expand our collection of games, providing endless opportunities for wordplay and enjoyment. Get ready to dive in and let the games begin!</span>.
       </p>
 
-      <p className="mb-8 ">
-        <span className="italic">When I&apos;m not coding</span>, I enjoy traveling, going to the gym, and going on runs. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently learning {" "}
-        <span className="font-medium">japanese and have been for 5 years</span>. I&apos;m also learning about Machine Learning/ AI.
-      </p>
     </section>
+    <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"></div>
+
+    <div className="min-h-screen flex flex-col lg:flex-row items-center p-8 sm:p-16 md:p-24 justify-center">
+      {/* Event Showcase  */}
+      <div data-theme="teal" className="mx-auto max-w-6xl">
+        <h2 className="sr-only">Featured case study</h2>
+        <section className="font-sans text-white">
+          <div className="lg:flex lg:items-center">
+            <div className="lg:w-1/2 flex justify-center lg:justify-end">
+              {" "}
+              {/* Image */}
+              <div>
+                <div className="h-full">
+                  <article className="h-full">
+                    <div className="h-full">
+                      <div className="relative cursor-pointer hover:brightness-110">
+                        
+                      </div>
+                      
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 bg-gray lg:flex-1">
+              {" "}
+              {/* Text */}
+              <div>
+                <div className="leading-relaxed">
+                  <h2 className="leading-tight text-4xl font-bold">
+                    Portfolio
+                  </h2>
+                  <p className="mt-4">
+                    A new portfolio updated using the mostup to date and modern design standards and methods.
+                  </p>
+                  <p className="mt-4">
+                    It will cost $16 to be an official student member
+                    of IEEE. You can join by clicking the link&nbsp;
+                    <a
+                      className="group inline-block px-3 py-1 bg-black text-yellow rounded-full border border-black hover:border-gray-950 hover:scale-110 transition duration-300"
+                      href="https://michael-mehrdadi-personal-portfolio.vercel.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Here
+                    </a>
+                    . &nbsp; Once a member, be sure to visit our
+                    events page for more information on upcoming
+                    opportunities.
+                  </p>
+                  <p>
+                    <a
+                      href="/events"
+                      className="mt-2 group bg-black text-white px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                    >
+                      Explore Events{" "}
+                      <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+
     <div className="bg-blue-600 my-24 h-1 w-1/2 rounded-full  sm:block "></div>
 
   </div>
