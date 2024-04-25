0afe30479c66c2a7d08ab7e7abe1fd50fce2d7ed
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-24 17:41:07 -0700

    Crossword init

--- src/components/Crossword/crossword.tsx
+++ src/components/Crossword/crossword.tsx
@@ -1,4 +1,71 @@
 import Sidebar from '../SideBar/sidebar'
+import Crossword, {
+  CrosswordImperative,
+  CrosswordGrid,
+  CrosswordProps,
+  CrosswordProvider,
+  CrosswordProviderImperative,
+  CrosswordProviderProps,
+  DirectionClues,
+  useIpuz,
+} from '@jaredreisinger/react-crossword';
+import styled from 'styled-components';
+
+const data = {
+  across: {
+    1: {
+      clue: 'one plus one',
+      answer: 'TWO',
+      row: 0,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: 'three minus two',
+      answer: 'ONE',
+      row: 0,
+      col: 2,
+    },
+  },
+};
+
+const CrosswordWrapper = styled.div`
+  max-width: 30em;
+
+  /* and some fun making use of the defined class names */
+  .crossword.correct {
+    rect {
+      stroke: rgb(100, 200, 100) !important;
+    }
+    svg > rect {
+      fill: rgb(100, 200, 100) !important;
+    }
+    text {
+      fill: rgb(100, 200, 100) !important;
+    }
+  }
+
+  .clue.correct {
+    ::before {
+      content: '\u2713'; /* a.k.a. checkmark: ✓ */
+      display: inline-block;
+      text-decoration: none;
+      color: rgb(100, 200, 100);
+      margin-right: 0.25em;
+    }
+
+    text-decoration: line-through;
+    color: rgb(130, 130, 130);
+  }
+`;
+const CrosswordMessageBlock = styled.div`
+  margin: 2em 0 4em;
+  display: flex;
+  gap: 2em;
+  max-height: 20em;
+`;
+
 
 function crossword() {
   return (
@@ -22,6 +89,33 @@ function crossword() {
           </button>
         </a>
       </div>
+
+      <CrosswordMessageBlock>
+        <CrosswordWrapper>
+          <Crossword
+            data={{
+              across: {
+                1: {
+                  clue: 'one plus one',
+                  answer: 'TWO',
+                  row: 0,
+                  col: 0,
+                },
+              },
+              down: {
+                2: {
+                  clue: 'opposite of "off"',
+                  answer: 'ON',
+                  row: 0,
+                  col: 2,
+                },
+              },
+            }}
+            theme={{ allowNonSquare: true }}
+            storageKey="third-example"
+          />
+        </CrosswordWrapper>
+      </CrosswordMessageBlock>
     </div>
   );
 }
