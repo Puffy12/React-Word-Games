c32f7c3a65bc9ffb5b4098b2e1fc8f5e3cea8fd9
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-04-25 15:27:04 -0700

    Update Crossword.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { faW, faHouse, faQuestionCircle, faS, faH } from '@fortawesome/free-solid-svg-icons'; //faMagnifyingGlass, faMemory, faTerminal, faBoxOpen, faW, faFingerprint,
+import { faW, faHouse, faQuestionCircle, faH, faC } from '@fortawesome/free-solid-svg-icons'; //faMagnifyingGlass, faMemory, faTerminal, faBoxOpen, faW, faFingerprint,
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Sidebar from '../SideBar/sidebar';
 import "./Home.css"; // Import your CSS file
@@ -20,7 +20,7 @@ const HomeMenu = () => {
                 <FontAwesomeIcon icon={faW} size="3x" className="icon" />
               </Link>
               <Link className="challenge-item " to={"/crossword"}>
-                <FontAwesomeIcon icon={faS} size="3x" className="icon" />
+                <FontAwesomeIcon icon={faC} size="3x" className="icon" />
               </Link>
               <Link className="challenge-item " to={"/hangman"}>
                 <FontAwesomeIcon icon={faH} size="3x" className="icon" />
