0fe1589c753602872d3101d091905cad762d4e1c
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-05-29 11:57:48 +0900

    Update miniData.tsx

--- src/components/MiniCrossword/miniData.tsx
+++ src/components/MiniCrossword/miniData.tsx
@@ -359,3 +359,19 @@ export const getCrosswordDataByIndex = (index: number) => {
   }
   return crosswordDataArray[index];
 };
+
+export const example = {
+  across: {
+    1: { clue: '', answer: '', row: 0, col: 0 },
+    4: { clue: '', answer: '', row: 1, col: 0 },
+    6: { clue: '', answer: '', row: 2, col: 0 },
+    7: { clue: '', answer: '', row: 3, col: 0 },
+    8: { clue: '', answer: '', row: 4, col: 0 },
+  },
+  down: { 
+    1: { clue: '', answer: '', row: 0, col: 0 },
+    2: { clue: '', answer: '', row: 0, col: 1 },
+    3: { clue: '', answer: '', row: 0, col: 2 },
+    4: { clue: '', answer: '', row: 0, col: 3 },
+    5: { clue: '', answer: '', row: 0, col: 4 },  },
+} as const;
