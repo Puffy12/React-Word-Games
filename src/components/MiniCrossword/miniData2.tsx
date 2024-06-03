8aeb8b92085f8dd53305858565517a0fe242d926
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-06-03 09:48:47 +0900

    Update miniData2.tsx

--- src/components/MiniCrossword/miniData2.tsx
+++ src/components/MiniCrossword/miniData2.tsx
@@ -1,30 +1,46 @@
 export const data = {
-    across: {
-      1: { clue: 'Exactly right', answer: '', row: 0, col: 0 },
-      7: { clue: 'Jenna who plays Wednesday Addams on TVs "Wednesday"', answer: '', row: 1, col: 0 },
-      8: { clue: 'Stick in a Bloody Mary', answer: 'CELERY', row: 2, col: 0 },
-      9: { clue: 'Bonobos, e.g.', answer: '', row: 3, col: 2 },
-      10: { clue: 'Something for a wizard or a T.S.A. agent', answer: '', row: 4, col: 0 },
-      12: { clue: 'Not running late', answer: '', row: 5, col: 0 },
-      15: { clue: 'Cutlery for cutting', answer: '', row: 6, col: 0 },
-      16: { clue: 'Leave a union', answer: '', row: 7, col: 0 },
+  across: {
+    1: { clue: 'Exactly right', answer: '', row: 0, col: 0 },
+    7: { clue: 'Jenna who plays Wednesday Addams on TVs "Wednesday"', answer: '', row: 1, col: 0 },
+    8: { clue: 'Stick in a Bloody Mary', answer: 'CELERY', row: 2, col: 0 },
+    9: { clue: 'Bonobos, e.g.', answer: '', row: 3, col: 2 },
+    10: { clue: 'Something for a wizard or a T.S.A. agent', answer: '', row: 4, col: 0 },
+    12: { clue: 'Not running late', answer: '', row: 5, col: 0 },
+    15: { clue: 'Cutlery for cutting', answer: '', row: 6, col: 0 },
+    16: { clue: 'Leave a union', answer: '', row: 7, col: 0 },
 
-    },
-    down: { 
-      1: { clue: 'Nurses workmate, informally', answer: '', row: 0, col: 0 },
-      2: { clue: 'Poets "before"', answer: '', row: 0, col: 1 },
-      3: { clue: 'It covers about one-fifth of the earths surface', answer: '', row: 0, col: 2 },
-      4: { clue: 'Trip down the rabbit hole on a subject', answer: '', row: 0, col: 3 },
-      5: { clue: 'Fearsome fairy tale monster', answer: '', row: 0, col: 4 },  },
-      6: { clue: '"No" votes', answer: '', row: 0, col: 5 },
-      10: { clue: 'Pans in Chinese cooking', answer: '', row: 4, col: 0 },
-      11: { clue: 'Diarist Frank', answer: '', row: 4, col: 1 },
-      13: { clue: 'Size above small: Abbr.', answer: 'MED', row: 5, col: 4 },
-      14: { clue: 'Suffix with Japan or Vietnam', answer: 'ESE', row: 5, col: 5 },
+  },
+  down: { 
+    1: { clue: 'Nurses workmate, informally', answer: '', row: 0, col: 0 },
+    2: { clue: 'Poets "before"', answer: '', row: 0, col: 1 },
+    3: { clue: 'It covers about one-fifth of the earths surface', answer: '', row: 0, col: 2 },
+    4: { clue: 'Trip down the rabbit hole on a subject', answer: '', row: 0, col: 3 },
+    5: { clue: 'Fearsome fairy tale monster', answer: '', row: 0, col: 4 },  },
+    6: { clue: '"No" votes', answer: '', row: 0, col: 5 },
+    10: { clue: 'Pans in Chinese cooking', answer: '', row: 4, col: 0 },
+    11: { clue: 'Diarist Frank', answer: '', row: 4, col: 1 },
+    13: { clue: 'Size above small: Abbr.', answer: 'MED', row: 5, col: 4 },
+    14: { clue: 'Suffix with Japan or Vietnam', answer: 'ESE', row: 5, col: 5 },
 
-  } as const;
+} as const;
 
-const crosswordDataArray = [data];
+export const data2 = {
+  across: {
+    1: { clue: 'Fezes and fedoras', answer: '', row: 0, col: 1 },
+    5: { clue: 'Trixie and ___, drag queen comedy duo', answer: '', row: 1, col: 0 },
+    6: { clue: 'Take in, as a cat', answer: '', row: 2, col: 0 },
+    7: { clue: 'Vehicle for prom night', answer: '', row: 3, col: 0 },
+    8: { clue: 'Aliens, for short', answer: '', row: 4, col: 0 },
+  },
+  down: { 
+    1: { clue: '"Ive ___ up to here!"', answer: '', row: 0, col: 1 },
+    2: { clue: 'Tiny building blocks', answer: '', row: 0, col: 2 },
+    3: { clue: 'What you might pass off a misspelling as', answer: '', row: 0, col: 3 },
+    4: { clue: 'Took a chair', answer: '', row: 0, col: 4 },
+    5: { clue: 'Hearty leafy green', answer: '', row: 1, col: 0 },  },
+} as const;
+
+const crosswordDataArray = [data, data2];
 
 export const getRandomCrosswordData2 = (index: number) => {
   let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
