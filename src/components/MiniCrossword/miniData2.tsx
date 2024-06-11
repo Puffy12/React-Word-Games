f5628936dffed33e3580ed9010feb62c594a44c0
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-06-11 13:07:00 +0900

    Update miniData2.tsx

--- src/components/MiniCrossword/miniData2.tsx
+++ src/components/MiniCrossword/miniData2.tsx
@@ -178,7 +178,23 @@ export const data10 = {
     5: { clue: '___ Jackson, former judge on "American Idol"', answer: 'RANDY', row: 0, col: 4 },  },
 } as const;
 
-const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10];
+export const data11 = {
+  across: {
+    1: { clue: 'In need of some cheering up', answer: '', row: 0, col: 1 },
+    4: { clue: 'City that holds an annual "Grand Prix de la baguette" contest', answer: '', row: 1, col: 0 },
+    6: { clue: 'Battery life, colloquially', answer: '', row: 2, col: 0 },
+    7: { clue: 'Take the wheel', answer: '', row: 3, col: 0 },
+    8: { clue: 'Twice-curved letter', answer: '', row: 4, col: 1 },
+  },
+  down: { 
+    1: { clue: 'Pan-fry', answer: '', row: 0, col: 1 },
+    2: { clue: 'Ram in the zodiac', answer: '', row: 0, col: 2 },
+    3: { clue: 'Cuts into small cubes', answer: '', row: 0, col: 3 },
+    4: { clue: 'Attire you might wear while working from home, for short', answer: '', row: 1, col: 0 },
+    5: { clue: 'Spanish verb meaning "to be"', answer: '', row: 1, col: 4 },  },
+} as const;
+
+const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11];
 
 export const getRandomCrosswordData2 = (index: number) => {
   let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
