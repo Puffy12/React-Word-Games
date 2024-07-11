b4a088db31cf0be61367aa37629043d366c39d03
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-11 16:44:03 +0900

    Update miniData4.tsx

--- src/components/MiniCrossword/miniData4.tsx
+++ src/components/MiniCrossword/miniData4.tsx
@@ -1,17 +1,17 @@
 export const data = {
 across: {
-    1: { clue: 'Part of a cherry you shouldnt eat', answer: '', row: 0, col: 0 },
-    5: { clue: '"Detour" sign feature', answer: '', row: 1, col: 0 },
-    7: { clue: 'Depart', answer: '', row: 2, col: 0 },
-    8: { clue: 'Nail polish brand that sounds like two letters of the alphabet', answer: '', row: 3, col: 0 },
-    9: { clue: 'Part of a pomegranate you should eat', answer: '', row: 4, col: 0 },
+    1: { clue: 'Part of a cherry you shouldnt eat', answer: 'STEM', row: 0, col: 0 },
+    5: { clue: '"Detour" sign feature', answer: 'ARROW', row: 1, col: 0 },
+    7: { clue: 'Depart', answer: 'LEAVE', row: 2, col: 0 },
+    8: { clue: 'Nail polish brand that sounds like two letters of the alphabet', answer: 'ESSIE', row: 3, col: 0 },
+    9: { clue: 'Part of a pomegranate you should eat', answer: 'SEED', row: 4, col: 1 },
 },
 down: { 
-    1: { clue: 'Shoppers excuse to "save money"', answer: '', row: 0, col: 0 },
-    2: { clue: 'Head lock?', answer: '', row: 0, col: 1 },
-    3: { clue: 'Wipe, as a memory card', answer: '', row: 0, col: 2 },
-    4: { clue: 'Common date night activity', answer: '', row: 0, col: 3 },
-    5: { clue: 'Fast-growing plant', answer: '', row: 0, col: 4 },  
+    1: { clue: 'Shoppers excuse to "save money"', answer: 'SALE', row: 0, col: 0 },
+    2: { clue: 'Head lock?', answer: 'TRESS', row: 0, col: 1 },
+    3: { clue: 'Wipe, as a memory card', answer: 'ERASE', row: 0, col: 2 },
+    4: { clue: 'Common date night activity', answer: 'MOVIE', row: 0, col: 3 },
+    6: { clue: 'Fast-growing plant', answer: 'WEED', row: 1, col: 4 },  
 },
 } as const;
 
