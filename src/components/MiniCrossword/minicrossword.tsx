aa327eeb4be66d7e441813bcfa3082641ff8fb8b
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-25 10:29:47 -0700

    July 25th miniData

--- src/components/MiniCrossword/minicrossword.tsx
+++ src/components/MiniCrossword/minicrossword.tsx
@@ -12,7 +12,7 @@ import { getCrosswordDataByIndex4 } from './miniData4';
 
 function MiniCrossword() {
   const crossword = useRef<CrosswordImperative>(null);
-  const data = getCrosswordDataByIndex4(10); 
+  const data = getCrosswordDataByIndex4(11); 
 
   const fillAllAnswers = useCallback<React.MouseEventHandler>(() => {
     crossword.current?.fillAllAnswers();
