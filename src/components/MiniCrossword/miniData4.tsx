bd4e38f2d51f58931c357f11f092126130372a89
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-31 09:49:46 -0700

    Update miniData4.tsx

--- src/components/MiniCrossword/miniData4.tsx
+++ src/components/MiniCrossword/miniData4.tsx
@@ -321,7 +321,24 @@ export const data18 = {
     },
 } as const;
 
-const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18];
+export const data19 = {
+    across: {
+        1: { clue: 'Keyboard key near the top left', answer: '', row: 0, col: 0 },
+        4: { clue: 'Hot couple, in a celebrity gossip column', answer: '', row: 1, col: 0 },
+        6: { clue: 'Wild, as an animal', answer: '', row: 2, col: 0 },
+        8: { clue: 'Like many four-legged friends', answer: '', row: 3, col: 0 },
+        9: { clue: 'Keyboard key on the bottom', answer: '', row: 4, col: 0 },
+    },
+    down: {
+        1: { clue: 'Little spats', answer: '', row: 0, col: 0 },
+        2: { clue: 'Thoroughly devoured', answer: '', row: 0, col: 1 },
+        3: { clue: 'Yogi who said "Always go to other people\'s funerals, otherwise they won\'t come to yours"', answer: '', row: 0, col: 2 },
+        5: { clue: 'Comedian Maron', answer: '', row: 0, col: 3 },
+        7: { clue: 'Caustic chemical compound', answer: '', row: 0, col: 4 },
+    },
+} as const;
+
+const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19];
 
 export const getRandomCrosswordData4 = (index: number) => {
     let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
