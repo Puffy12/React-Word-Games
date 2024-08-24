d7cb519f155cf77641894293dadad0e71be8a4a6
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-08-24 11:16:21 -0700

    Update miniData6.tsx

--- src/components/MiniCrossword/MiniData/miniData6.tsx
+++ src/components/MiniCrossword/MiniData/miniData6.tsx
@@ -15,7 +15,29 @@ export const data = {
     },
 } as const;
 
-const crosswordDataArray = [data];
+export const data2 = {
+    across: {
+        1: { clue: 'Eight: Prefix', answer: 'OCTO', row: 0, col: 0 },
+        11: { clue: 'Like residents of Kathmandu', answer: 'FROZE', row: 1, col: 0 },
+        12: { clue: 'Casually drink', answer: 'FOREST', row: 2, col: 0 },
+        13: { clue: 'The "1" of $0.01', answer: 'SWIMCAP', row: 3, col: 0 },
+        5: { clue: 'Got stuck, as a video', answer: 'NEPALI', row: 4, col: 1 },
+        7: { clue: 'Over 90% of Maine and 80% of New Hampshire', answer: 'SIPON', row: 5, col: 2 },
+        9: { clue: 'Coverage of the Summer Olympics?', answer: 'CENT', row: 6, col: 3 },
+    },
+    down: {
+        1: { clue: 'Turn-___ (unattractive qualities)', answer: 'OFFS', row: 0, col: 0 },
+        10: { clue: 'Ice cream purchase', answer: 'CROWN', row: 0, col: 1 },
+        2: { clue: 'Symbol in the Budweiser and Corona logos', answer: 'TORIES', row: 0, col: 2 },
+        3: { clue: 'Defeated party in the recent U.K. elections', answer: 'OZEMPIC', row: 0, col: 3 },
+        4: { clue: 'Popular weight loss drug', answer: 'ESCAPE', row: 1, col: 4 },
+        6: { clue: 'Ford S.U.V.', answer: 'TALON', row: 2, col: 5 },
+        8: { clue: 'Eagle\'s claw', answer: 'PINT', row: 3, col: 6 },
+    },
+} as const;
+
+
+const crosswordDataArray = [data, data2];
 
 export const getRandomCrosswordData6 = (index: number) => {
     let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
