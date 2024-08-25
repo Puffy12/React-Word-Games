c26d4ea603fb62b66af48e3775d598f67f683a93
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-08-25 14:02:50 -0700

    Aug 25th miniData

--- src/components/MiniCrossword/MiniData/miniData6.tsx
+++ src/components/MiniCrossword/MiniData/miniData6.tsx
@@ -53,7 +53,7 @@ export const data3 = {
     },
 } as const;
 
-const crosswordDataArray = [data, data2];
+const crosswordDataArray = [data, data2, data3];
 
 export const getRandomCrosswordData6 = (index: number) => {
     let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
