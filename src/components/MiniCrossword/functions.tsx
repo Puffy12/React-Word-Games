148fd5d891d7833e9753d05bab687f769ce3d5e8
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-09-23 10:45:26 -0700

    Sept 23rd miniData

--- src/components/MiniCrossword/functions.tsx
+++ src/components/MiniCrossword/functions.tsx
@@ -19,7 +19,7 @@ const crosswordDataArrays = [
 ];
 
 export const handleMiniDataFetch = (index: number) => {
-    const todaysIndex = 10; //Changes based on the current days Mini Crossword
+    const todaysIndex = 11; //Changes based on the current days Mini Crossword
     if(index === 0){
         const data = getCrosswordDataByIndex7(todaysIndex);
         return data;
