0af824c6dd64f8c1f8c31d7f8698e1ea3d39cbb5
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-11-01 15:01:34 -0700

    Update miniData9.tsx

--- src/components/MiniCrossword/MiniData/miniData9.tsx
+++ src/components/MiniCrossword/MiniData/miniData9.tsx
@@ -3,17 +3,18 @@ export const data = {
         1: { clue: 'With 5-Down, rhyming Japanese philosophy of finding beauty in imperfection', answer: 'WABI', row: 0, col: 1 },
         5: { clue: 'Sneaker bottoms', answer: 'SOLES', row: 1, col: 0 },
         6: { clue: 'Typeface similar to Helvetica', answer: 'ARIAL', row: 2, col: 0 },
-        7: { clue: 'Piece of grass', answer: 'ARIAL', row: 2, col: 0 },
+        7: { clue: 'Piece of grass', answer: 'BLADE', row: 3, col: 0 },
         8: { clue: 'Personal items checked by bouncers', answer: 'IDS', row: 4, col: 0 },
     },
     down: {
         1: { clue: 'Disney ___', answer: 'WORLD', row: 0, col: 1 },
-        2: { clue: 'Pen name', answer: 'ALIAS', row: 0, col: 2 },
-        3: { clue: 'Pearl in a pearl necklace, e.g.', answer: 'BEAD', row: 0, col: 3 },
+        2: { clue: 'Pen name', answer: 'WORLD', row: 0, col: 1 },
+        3: { clue: 'Pearl in a pearl necklace, e.g.', answer: 'WORLD', row: 0, col: 1 },
         4: { clue: 'Bit of land in the ocean', answer: 'ISLE', row: 0, col: 4 },
         5: { clue: 'See 1-Across', answer: 'SABI', row: 1, col: 0 },
     },
 } as const;
+
 
 
 const crosswordDataArray = [data];
