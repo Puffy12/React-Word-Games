7c231be91606585cccd5269f1b35a7991bf6a462
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-11-24 12:10:00 -0800

    Update miniData9.tsx

--- src/components/MiniCrossword/MiniData/miniData9.tsx
+++ src/components/MiniCrossword/MiniData/miniData9.tsx
@@ -332,7 +332,24 @@ export const data19 = {
     },
 } as const;
 
-const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19];
+export const data20 = {
+    across: {
+        1: { clue: 'Setting for the Emmy-winning series "Shogun"', answer: 'JAPAN', row: 0, col: 0 },
+        6: { clue: 'Part of a dictionary entry', answer: 'USAGE', row: 1, col: 0 },
+        7: { clue: '"Got it!," on a walkie-talkie', answer: 'ROGER', row: 2, col: 0 },
+        8: { clue: 'How staticky hair stands', answer: 'ONEND', row: 3, col: 0 },
+        9: { clue: 'Takes a break', answer: 'RESTS', row: 4, col: 0 },
+    },
+    down: {
+        1: { clue: 'One of 12 in a courtroom', answer: 'JUROR', row: 0, col: 0 },
+        2: { clue: 'In unison', answer: 'ASONE', row: 0, col: 1 },
+        3: { clue: 'Things dog-eared in a beloved book', answer: 'PAGES', row: 0, col: 2 },
+        4: { clue: 'F.B.I. worker', answer: 'AGENT', row: 0, col: 3 },
+        5: { clue: 'Colorful candy with a Gummy Clusters variety', answer: 'NERDS', row: 0, col: 4 },
+    },
+} as const;
+
+const crosswordDataArray = [data, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20];
 
 export const getRandomCrosswordData9 = (index: number) => {
     let randomIndex = Math.floor(Math.random() * crosswordDataArray.length);
